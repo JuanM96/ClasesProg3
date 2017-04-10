e595934153f1614446df7d70114cd68d03ffe25e
alumno <alumno>
2017-04-10 20:34:45 -0300

    Clase 04 $_Files Subida y movida

--- Clases/Clase4/Practica/php/archivo.php
+++ Clases/Clase4/Practica/php/archivo.php
@@ -1,4 +1,16 @@
 <?php
-$destino = "../uploads/".$_FILES['archivo']['name'];
-move_uploaded_file($_FILES["archivo"]["tmp_name"], $destino);
+$destinoUp = "../uploads/".$_FILES['archivo']['name'];
+$backup = "../backup/".date("d m y")."-".$_FILES['archivo']['name'];
+if(!file_exists($destinoUp))
+{
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $destinoUp);
+    echo "<img src=".$destinoUp.">";
+}
+else
+{
+    copy($destinoUp,$backup);
+    
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $destinoUp);
+}
+
 ?>
