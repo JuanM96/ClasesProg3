39e81d0845ec6d60d16df07b1f86a29107ce5191
alumno <alumno>
2017-04-17 20:27:07 -0300

    Clase 5 y TP Mod

--- TP/php/mostrarmod.php
+++ TP/php/mostrarmod.php
@@ -1,21 +1,22 @@
 <?php
-include_once("Empleado.php");
-$archivo = fopen("empleado.txt","r");
-$cont = 0;
-echo "<script src='../script/modificar.js'></script>";
-echo "<select name='empleados' size='20'>";
-while (!feof($archivo)) {
-    $cont += 1;
-    $linea = fgets($archivo);
-    $arrayEmpleado = explode("-",$linea);
-    if (count($arrayEmpleado) != 1) {
-        echo "<option>".$arrayEmpleado[2]." - ".$arrayEmpleado[1]."</option>";
-    }
+// include_once("Empleado.php");
+// $archivo = fopen("empleado.txt","r");
+// $cont = 0;
+// echo "<script src='../script/modificar.js'></script>";
+// echo "<select name='empleados' size='20'>";
+// while (!feof($archivo)) {
+//     $cont += 1;
+//     $linea = fgets($archivo);
+//     $arrayEmpleado = explode("-",$linea);
+//     if (count($arrayEmpleado) != 1) {
+//         echo "<option value = ".$cont.">".$arrayEmpleado[2]." - ".$arrayEmpleado[1]."</option>";
+//     }
     
-}
-echo "</select><br>";
-echo "<input type='submit' value='Aceptar' onclick='EmpleadoSelecto()'><br>";
-echo "<a href='../index.html'>Volver a Inicio</a><br>";
+// }
+// echo "</select><br>";
+// echo "<input type='submit' value='Aceptar' onclick='EmpleadoSelecto()'><br>";
+// echo "<a href='../index.html'>Volver a Inicio</a><br>";
 
-fclose($archivo); 
+// fclose($archivo);
+
 ?>
