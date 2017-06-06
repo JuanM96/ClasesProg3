c904346bb0e2e563a82f794ee013d805e6283773
alumno <alumno>
2017-06-05 21:19:31 -0300

    Clase 8 Slim parte 2 y Correccion de bug en pagina del tp

---
+++ Clases/Clase8parte2/php/index.php
@@ -0,0 +1,21 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require '../clases/AccesoDatos.php';
+
+$app = new \Slim\App;
+$app->post('/guardarempleado', function (Request $request, Response $response) {
+    
+    $files = $request->getUploadedFiles();
+    $targetPath = "../img/".$files['archivo']->getClientFilename();
+    //$response->getBody()->write(var_dump($files));
+    $files['archivo']->moveTo($targetPath);
+    return $response;
+});
+$app->run();
+
+
+
+?>
---
+++ Clases/Clase8parte2/script/script.js
@@ -0,0 +1,21 @@
+// $(document).ready(function () {
+
+//     // https://preloaders.net/
+
+//     $("#btnsubir").click(function (event) {
+//         $.ajax({
+//             url: 'http://localhost:8080/repprog/Clases/Clase8parte2/index.php/guardarempleado',
+//             type: 'POST',
+//             data: { nombre: num1, file: num2 },
+//             dataType: 'text',
+//             success: function (data, textStatus, p) {
+
+//                 $("#resultado").text(data);
+//             }
+
+//         })
+//             .fail(function (p, ts, et) {
+
+//             })
+//     });
+// });
